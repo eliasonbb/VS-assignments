905e798dba54c6135a80f7001dd0e53c07ffe6fe
eliasonbb <eliason.bb@gmail.com>
2022-04-24 14:46:46 -0600

    grocery-store and loop olympics start

---
+++ module-1/grocery-store/index.js
@@ -0,0 +1,11 @@
+var shopper ={
+    firstName: "john",
+    lastName:"deere",
+    age: 12,
+    gender: "male",
+    hungry: true,
+    fullName: function(){
+        return this.firstName + " " + this.lastName;
+    },
+    groceryCart:["beans", "mop", "wombat"]
+}
---
+++ module-1/loop-olympics/index.js
@@ -0,0 +1,93 @@
+// ///////////////////////////////
+// console.log("****PRELIMS****")
+// for(var i = 0; i<= 9; i++){
+//     console.log(i);
+// }
+
+// console.log("------")
+
+// for(var j = 9; j>=0; j--){
+//     console.log(j);
+// }
+
+// console.log("------")
+
+// const fruit = ["banana", "orange", "apple", "kiwi"]
+// for(var f = 0; f< fruit.length; f++){
+//     console.log(fruit[f])
+// }
+// //////////////////////////////
+// console.log("****BRONZE****")
+// const numbers = []
+// for(var k=0; k<=9; k++){
+//     numbers.push(k)
+// }
+// console.log(numbers)
+// console.log("------")
+// for(var e=0;e<100;e++){
+//     if(e%2==0){
+//         console.log(e)
+//     }
+// }
+// console.log("------")
+// const fruit2 = ["banana", "orange", "apple", "kiwi", "pear", "peach"]
+// const newfruit=[]
+// for(fr = 0; fr<fruit2.length; fr++){
+//     if(fr%2!=0){
+//         newfruit.push(fruit2[fr])
+//     }
+// }
+// console.log(newfruit)
+// console.log("****SILVER****")
+// const peopleArray = [
+//     {
+//       name: "Harrison Ford",
+//       occupation: "Actor"
+//     },
+//     {
+//       name: "Justin Bieber",
+//       occupation: "Singer"
+//     },
+//     {
+//       name: "Vladimir Putin",
+//       occupation: "Politician"
+//     },
+//     {
+//       name: "Oprah",
+//       occupation: "Entertainer"
+//     }
+//   ]
+
+// for(n=0; n<peopleArray.length;n++){
+//     console.log(peopleArray[n].name)
+// }
+// console.log("------")
+
+// const names = []
+// const occupations = []
+// for(var on=0; on<peopleArray.length; on++){
+//     names.push(peopleArray[on].name)
+//     occupations.push(peopleArray[on].occupation)
+// }
+// console.log(names, occupations)
+// console.log("------")
+// const eoNames =[]
+// const eoOccupations =[]
+// for(var eo=0; eo<peopleArray.length;eo++){
+//     if(eo%2==0){
+//         eoNames.push(peopleArray[eo].name)
+//     }else{
+//         eoOccupations.push(peopleArray[eo].occupation)
+//     }
+// }
+// console.log(eoNames, eoOccupations)
+console.log("****GOLD****")
+const firstG = []
+const ifirstG = []
+for(var fg=0; fg<3;fg++){
+    for(var ifg=0; ifg<3; ifg++){
+        ifirstG.push()
+    }
+    firstG.push(ifirstG)
+}
+console.log(ifirstG)
